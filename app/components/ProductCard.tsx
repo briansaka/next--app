de9d447b35afd11a8aa77a13ff39501f593aa424
briansaka <kipkiruicheruiyot41@gmail.com>
2025-01-23 14:19:50 +0300

    Adds tailwind csscacheing

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,14 +1,17 @@
 
 
 import React from 'react'
-import AddToCart from './AddToCart'
+import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css';
+
+styles.card
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className= {styles.card}>
         <AddToCart/>
     </div>
   )
 }
 
-export default ProductCard
+export default ProductCard  
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,11 +7,12 @@ interface User {
 }
 
 const Userspage = async () => {
-   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+   const res = await fetch('https://jsonplaceholder.typicode.com/users',{cache:'no-store'});
    const users: User[] = await res.json();
   return (
     <>
     <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
     <ul>
         {users.map(user=> <li key={user.id}>{user.name}</li>)}
     </ul>
